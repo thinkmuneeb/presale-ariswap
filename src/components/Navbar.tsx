efe6a37a7e6754d0944616cad76a36051e6c0e43
Muneeb <thinkmuneeb@gmail.com>
2021-07-13 11:42:21 +0500

    getting a value from smart contract

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import Button from "../components/Button";
 import Icon from "../assets/logo.png";
 import ConnectModal from "./Modal/ConnectModal";
 import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
 
 const requiredNetwork = "Ropsten";
 const requiredChainId = "0x3";
@@ -79,10 +80,19 @@ const Navbar = () => {
                   method: "eth_chainId",
                 });
 
-                if (chainId !== requiredChainId) {
+                if (chainId === requiredChainId) {
+                  const web3 = new Web3(provider);
+                  const presale = new web3.eth.Contract(
+                    JSON.parse(
+                      '[{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"contract IERC20","name":"_usdt","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"uint256","name":"_tokens","type":"uint256"}],"name":"buyTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"getTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rate","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_rate","type":"uint256"}],"name":"setRate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"token","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokensSold","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"usdt","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"wallet","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]'
+                    ),
+                    "0xa27fFC06B51c16FD4f34235F66F788E7a640eaff"
+                  );
+                  const rate = await presale.methods.rate().call();
+                  console.log({ rate });
+                } else {
                   alert("Please switch network to " + requiredNetwork);
                 }
-                // console.log({ acc, chainId });
               } else {
                 alert("Please install MetaMask!");
               }
