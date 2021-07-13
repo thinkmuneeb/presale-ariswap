f9875478bf04c59dfcbfb08a9a85797442b391e6
Muneeb <thinkmuneeb@gmail.com>
2021-07-13 12:02:52 +0500

    show address as 0x123***7e11

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,15 @@ const IconImage = styled.img`
 const Navbar = () => {
   const [modalShow, setModalShow] = useState(false);
   const [height, setHeight] = useState(0);
+  const [connectButtonText, setConnectButtonText] = useState("Connect");
 
+  const showAddress = (_address: any) => {
+    return (
+      _address.substr(0, 4) +
+      "****" +
+      _address.substr(_address.length - 4, _address.length)
+    );
+  };
   const handleScroll = (e: any) => {
     setHeight(window.pageYOffset);
   };
@@ -81,17 +89,18 @@ const Navbar = () => {
                 });
 
                 if (chainId === requiredChainId) {
-                  const web3 = new Web3(provider);
-                  const presale = new web3.eth.Contract(
-                    JSON.parse(
-                      '[{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"contract IERC20","name":"_usdt","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"uint256","name":"_tokens","type":"uint256"}],"name":"buyTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"getTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rate","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_rate","type":"uint256"}],"name":"setRate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"token","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokensSold","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"usdt","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"wallet","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]'
-                    ),
-                    "0xa27fFC06B51c16FD4f34235F66F788E7a640eaff"
-                  );
-                  const rate = await presale.methods
-                    .buyTokens("100")
-                    .send({ from: acc[0] });
-                  console.log({ rate });
+                  setConnectButtonText("Connected: " + showAddress(acc[0]));
+                  // const web3 = new Web3(provider);
+                  // const presale = new web3.eth.Contract(
+                  //   JSON.parse(
+                  //     '[{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"contract IERC20","name":"_usdt","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"uint256","name":"_tokens","type":"uint256"}],"name":"buyTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"getTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rate","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_rate","type":"uint256"}],"name":"setRate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"token","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokensSold","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"usdt","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"wallet","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]'
+                  //   ),
+                  //   "0xa27fFC06B51c16FD4f34235F66F788E7a640eaff"
+                  // );
+                  // const rate = await presale.methods
+                  //   .buyTokens("100")
+                  //   .send({ from: acc[0] });
+                  // console.log({ rate });
                 } else {
                   alert("Please switch network to " + requiredNetwork);
                 }
@@ -99,7 +108,7 @@ const Navbar = () => {
                 alert("Please install MetaMask!");
               }
             }}
-            text="Connect"
+            text={connectButtonText}
           />
         </div>
       </div>
